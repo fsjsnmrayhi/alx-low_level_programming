db0d1a57d344050d370e6d440cbbde5a04d9e982
fsjsnmrayhi <nourmrayhi@gmail.com>
2023-07-14 07:57:57 -0700

    hjb

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,12 +8,12 @@
  **/
 int main(void)
 {char ch = 'a';
-char CH = 'A'
+char CH = 'A';
 while (ch <= 'z')
-	{
-	putchar(ch);
-	ch++;
-	}
+{
+putchar(ch);
+ch++;
+}
 while (CH <= 'Z')
 {
 putchar(CH);
