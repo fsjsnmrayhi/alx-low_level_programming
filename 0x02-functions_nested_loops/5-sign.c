f140e54fa7180fe536e12e127c6e2a64f363ad49
fsjsnmrayhi <nourmrayhi@gmail.com>
2023-07-18 16:08:19 -0700

    fhv

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,8 +5,8 @@
  * @n: the int to check
  * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
-*-1 and prints - if n is less than zero
- */
+ * -1 and prints - if n is less than zero
+*/
 int print_sign(int n)
 {
 	if (n > 0)
@@ -14,7 +14,7 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	} else if (n == 0)
-}
+	{
 		_putchar(48);
 		return (0);
 	} else if (n < 0)
